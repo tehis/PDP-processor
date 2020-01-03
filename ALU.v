7708e7c3282e8ebcec45c3e77805b0ba2851c1c1
tehis <ali.khoshtinattt@gmail.com>
2020-01-03 15:21:56 +0330

    Add multiplexers and fix ALU and PC

--- ALU.v
+++ ALU.v
@@ -2,10 +2,10 @@
 
 module ALU(input[11:0] in1, in2, cin, input[1:0] alu_op, output reg[11:0] alu_out);
     
-    always @(alu_op, in1, in1) begin
+    always @(alu_op, in1, in1)
         case(alu_op)
             0: alu_out = in1+in2;
             1: alu_out = in1 & in2;
-            default alu_out = 11'bz;'
-    end
+            default alu_out = 11'bz;
+        endcase
 endmodule
--- PC.v
+++ PC.v
@@ -1,12 +1,12 @@
 `timescale 1ns/1ns
-module PC(input clk, rst, load_pc, incr_pc, input[11:0] input_address, output[11:0] output_address);
+module PC(input clk, rst, load_pc, incr_pc, input[11:0] input_address, output reg[11:0] output_address);
     always @(posedge clk, posedge rst) begin
         if (rst)
             output_address <= 0;
-        else if (load_pd)
-            output_adress <= input_address;
+        else if (load_pc)
+            output_address <= input_address;
         else if (incr_pc)
-            pc_out <= pc_out+1;
+            output_address <= output_address+1;
     end
 endmodule
 
